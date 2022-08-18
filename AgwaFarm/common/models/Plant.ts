fec6c56189f78021f0be77d3dec628e9e4096cd8
Orr Eilam <orr@mnch.co>
2022-08-18 17:48:40 +0300

    created models for plant and category

--- AgwaFarm/common/models/Plant.ts
+++ AgwaFarm/common/models/Plant.ts
@@ -6,14 +6,42 @@ export interface IPlantCatagory {
     name: string;
     minSelection: number;
     maxSelection: number;
+    plants: IBasePlant[];
 }
 
 /**
  * A basic plant structure.
  */
-export interface IPlant {
+export interface IBasePlant {
     id: string;
     name: string;
-    imageURL: string;
-    catagory: IPlantCatagory;
+}
+
+/**
+ * An extended full-info version of a plant.
+ */
+export interface IFullPlantInfo extends IBasePlant {
+    imageID: string;
+    seedToCrop: string;
+    yield: string;
+    lifeCycle: string;
+    description: string;
+    nutritionFacts: INutritionInfo;
+}
+
+/**
+ * The basic structure of the nutritional value of a plant.
+ */
+export interface INutritionInfo {
+    portionInfo: string;
+    items: INutritionItemInfo[];
+}
+
+/**
+ * The basic structure of a nutritional item's info.
+ */
+export interface INutritionItemInfo {
+    key: string;
+    nutrientValue: string;
+    percentageOfRDA: string;
 }
